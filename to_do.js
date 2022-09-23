a5000af22b308f6696941db3fad12d7a90c4f6fd
Alex Paredes <alexparedes83@gmail.com>
2022-09-23 10:04:30 -0600

    made the render function in js. Check html

--- to_do.js
+++ to_do.js
@@ -72,3 +72,29 @@ const lists = {
     }//took out the comma. May need later in the future.
 }
 const currentList = list[0];
+
+function render(){
+    //Will hold the html from the sidebar
+    let listsHTML = '<ul class = "list-group">';
+    //Iterate through the lists to get the names
+    lists.forEach((list)=> {
+        listsHTML += `<li class = "list-group-item">${list.name}</li>`;
+    });
+
+    listsHTML += '</ul>';
+    //prints out lists
+
+    document.getElementById('lists').innerHTML = listsHTML;
+    //prints out the name of the current list
+
+    document.getElementById('current-list-name').innerText = currentList.name;
+    //iterate over the todos in the current list
+
+    let todosHtml = '<ul class = "list-group-flush">';
+    currentList.todos.forEach((list) => {
+        todosHtml += `<li class = "list-group-item">${todo.text}</li>`;
+    });
+    //print out the todos
+    document.getElementById('current-list-todos').innerHTML = todosHtml;
+}
+
